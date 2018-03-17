ff81fc7fcdc3cc145f409a907846980d16e71a0b
iamank1t <ankitsingh.t15@gmail.com>
2018-03-17 11:46:39 +0530

    Initial Commit

---
+++ Travel-IT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel-IT
+//
+//  Created by Ankit Singh on 17/03/18.
+//  Copyright © 2018 Ankit Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
