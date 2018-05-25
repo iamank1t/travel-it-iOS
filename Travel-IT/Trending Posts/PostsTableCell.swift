dfdbb0453b4c8611e9c08979a342c2929a938d3e
iamank1t <ankitsingh.t15@gmail.com>
2018-05-25 14:11:53 +0530

    changed ui of trending travel posts

--- Travel-IT/Trending Posts/PostsTableCell.swift
+++ Travel-IT/Trending Posts/PostsTableCell.swift
@@ -9,57 +9,57 @@
 import UIKit
 import SDWebImage
 class PostsTableCell: UITableViewCell {
-
+    
+    @IBOutlet var commentCountImageVw: UIImageView!
+    @IBOutlet var moneyImageVw: UIImageView!
+    @IBOutlet var upvoteCountImageVw: UIImageView!
     @IBOutlet var commentCountLabel: UILabel!
     @IBOutlet var moneyLabel: UILabel!
     @IBOutlet var upvoteCountLabel: UILabel!
-    @IBOutlet var postCardView: UIView!
     @IBOutlet var postMainImage: UIImageView!
     @IBOutlet var postTitle: UILabel!
     @IBOutlet var authorNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.postCardView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        self.postCardView.layer.shadowColor = UIColor.black.cgColor
-        self.postCardView.layer.shadowRadius = 4
-        self.postCardView.layer.shadowOpacity = 0.25
-        self.postCardView.layer.masksToBounds = false;
-        self.postCardView.clipsToBounds = false;
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     // function to update single cell data
-    func updateCellData(data: [String: AnyObject]) {
+    func updateCellData(data: [String: AnyObject]) { 
         self.postTitle.text = data["title"]! as? String
-        self.authorNameLabel.text = data["author"] as? String
+        if let author = data["author"] as? String {
+            self.authorNameLabel.text = "By : \(author))"
+        }
         let upvoteCount = data["net_votes"] as? Int
         self.upvoteCountLabel.text = "\(upvoteCount ?? 0)"
         let commentCount = data["children"] as? Int
         self.commentCountLabel.text = "\(commentCount ?? 0)"
         self.moneyLabel.text = data["pending_payout_value"] as? String
-        let metadata = data["json_metadata"]! as! String
-        let data = metadata.data(using: .utf8)!
-        do {
-            if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [String:AnyObject]
-            {
-                if let images = jsonArray["image"] as? [String] {
-                let finalImage = images[0]
-                if let url = URL(string: finalImage) {
-                    postMainImage.sd_setImage(with: url)
+        DispatchQueue.global(qos: .background).async {
+            let metadata = data["json_metadata"]! as! String
+            let data = metadata.data(using: .utf8)!
+            do {
+                if let jsonArray = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as? [String:AnyObject]
+                {
+                    if let images = jsonArray["image"] as? [String] {
+                        let finalImage = images[0]
+                        print(finalImage)
+                        if let url = URL(string: finalImage) {
+                            self.postMainImage.sd_setImage(with: url, placeholderImage: #imageLiteral(resourceName: "travel_placeholder_image"), options: SDWebImageOptions.highPriority, completed: nil)
+                        }
+                    }
+                } else {
+                    print("bad json")
                 }
-                }
-            } else {
-                print("bad json")
+            } catch let error as NSError {
+                print(error)
             }
-        } catch let error as NSError {
-            print(error)
         }
-        
     }
-
 }
